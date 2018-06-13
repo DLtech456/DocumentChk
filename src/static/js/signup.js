bd12f685df4c5c72a11181249df2c1c651ae8383
Venki S <venki_chipin@hotmail.com>
2018-06-12 22:13:06 -0500

    updates to image display

--- src/static/js/signup.js
+++ src/static/js/signup.js
@@ -1,51 +1,14 @@
-(function () {
-    var takePicture = document.querySelector("#take-picture"),
-        showPicture = document.querySelector("#show-picture"),
-        data        = $("#data");
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-    if (takePicture && showPicture) {
-        // Set events
-        takePicture.onchange = function (event) {
-            // Get a reference to the taken picture or chosen file
-            var files = event.target.files,
-                file;
-            if (files && files.length > 0) {
-                file = files[0];
-                try {
-                    // Get window.URL object
-                    var URL = window.URL || window.webkitURL;
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
 
-                    // Create ObjectURL
-                    var imgURL = URL.createObjectURL(file);
-
-                  	window.text = file;
-               		data.text(file);
-
-                    // Set img src to ObjectURL
-                    showPicture.src = imgURL;
-                  	$(showPicture).show();
-
-                    // Revoke ObjectURL
-                    //URL.revokeObjectURL(imgURL);
-                }
-                catch (e) {
-                    try {
-                        // Fallback if createObjectURL is not supported
-                        var fileReader = new FileReader();
-                        fileReader.onload = function (event) {
-                            showPicture.src = event.target.result;
-                        };
-                        fileReader.readAsDataURL(file);
-                    }
-                    catch (e) {
-                        // Display error message
-                        var error = document.querySelector("#error");
-                        if (error) {
-                            error.innerHTML = "Neither createObjectURL or FileReader are supported";
-                        }
-                    }
-                }
-            }
-        };
+            reader.readAsDataURL(input.files[0]);
+        }
     }
-})();
