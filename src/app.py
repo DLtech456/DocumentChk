91c7a1d6d4a8a51c7d99187f1b15daf877bdc118
Venki S <venki_chipin@hotmail.com>
2018-06-12 20:18:59 -0500

    added method in app.py

--- src/app.py
+++ src/app.py
@@ -12,6 +12,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def hello_world():
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+
+    file.save(filename)
+    seValues = ImageDetect.SightEngineCall(filename)
+    female = seValues[1]
+    face = seValues[2]
+
+    # obtain the OCR data
+    ocr = ImageDetect.ocr_space_file(filename)
+    p2 = ocr[1]
+    dLicense=ocr[2]
+
+    return render_template('uploaded.html', female=female, face=face, init=True, user_image=filename, ocr=p2,dLicense=dLicense)
+
 
 if __name__ == "__main__":
     app.run()
